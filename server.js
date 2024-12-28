124edcaba8e418b3e93c4fd8eed649fade2e32e6
Berni <pbernal@mendoza.edu.ar>
2024-12-28 12:46:25 -0300

    agregamos una ruta a navidad

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ app.get('/', function (req, res) {
     res.send('Bienvenidos a nuestro Primer Servidor con Express')
 })
 
+app.get('/navidad', function (req, res) {
+    res.send('Feliz Navidad y Prospero Año Nuevo')
+});
+
 app.listen(3000)
+
+
+
 
