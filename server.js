d29cd3016a68a873088e4c41374d27310064ca2e
Berni <pbernal@mendoza.edu.ar>
2025-01-04 12:23:01 -0300

    agregamos formulario

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express')
+const hbs = require('hbs');
 const app = express()
+const PORT = 3000
+
+
+app.set('view engine', 'hbs');
 
 app.get('/', function (req, res) {
     res.send('Bienvenidos a nuestro Primer Servidor con Express')
 })
 
 app.get('/navidad', function (req, res) {
-    res.send('Feliz Navidad y Prospero Año Nuevo')
+    res.render('index')
 });
 
-app.listen(3000)
+
+app.get('/formulario', function (req, res) {
+    res.render('formulario')
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`);
+    
+})
 
 
 
